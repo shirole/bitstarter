131816a3f1cb655c68bcee04f17dcd9cf58fa863
Sameer Shirole <shirole@hotmail.com>
2013-07-07 21:18:24 +0000

    Fixed web.js syntax error

--- web.js
+++ web.js
@@ -5,7 +5,8 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
  // response.send('Hello World2!');
- response.send(fs.readFileSync('index.html').toString));
+ response.send(fs.readFileSync('index.html').toString);
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
