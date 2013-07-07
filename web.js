0439dabf153c499c5c104219588e0a3535ad89e1
Sameer Shirole <shirole@hotmail.com>
2013-07-07 21:25:30 +0000

    used string in web.js

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
  // response.send('Hello World2!');
- response.send(fs.readFileSync('index.html').toString);
+ response.send(fs.readFileSync('index.html', 'utf-8'));
 });
 
 var port = process.env.PORT || 5000;
